e64e1ee2a0e008cabb7ef8da16b05acca1ba85b5
Dominic Farolino <domfarolino@gmail.com>
2023-05-21 21:47:13 -0400

    Reset `base::Thread::delegate_` on main thread only (#7)
    This commit resets the `reset()` of `base::Thread::delegate_` to be on
    the main thread only, instead of on the backing thread when it
    terminates. The previous design was bad because it meant the API
    surface of `base::Thread` (`GetTaskRunner()` specifically) could
    change unpredictably by the backing thread terminating itself. This new
    design ensures that `base::Thread::delegate_` is only ever mutated on
    the main thread, and clarifies that this is thread-safe because it is
    only ever reset after the backing thread is terminated/joined.
    
    Closes #1.

--- base/threading/thread_test.cc
+++ base/threading/thread_test.cc
@@ -89,7 +89,7 @@ TEST_P(ThreadTest, GetTaskRunnerBeforeStart) {
 TEST_P(ThreadTest, StartAfterThreadTerminatesButBeforeJoinOrStop) {
   std::shared_ptr<TaskLoop> delegate_reset_waiter =
     TaskLoop::Create(ThreadType::WORKER);
-  thread->RegisterDelegateResetCallbackForTesting(
+  thread->RegisterBackingThreadTerminateCallbackForTesting(
     delegate_reset_waiter->QuitClosure());
 
   thread->Start();
@@ -102,9 +102,11 @@ TEST_P(ThreadTest, StartAfterThreadTerminatesButBeforeJoinOrStop) {
   delegate_reset_waiter->Run();
 
   // At this point we know that the thread's delegate has been "quit" and the
-  // underlying thread has been entirely terminated. Delegate should be reset
-  // and therefore unable to produce task runners.
-  EXPECT_FALSE(thread->GetTaskRunner());
+  // underlying thread has been entirely terminated. The internal delegate,
+  // which vends per-base::Thread TaskRunners, however, does not get reset until
+  // `Stop()` is called, so it is able to produce TaskRunners for the underlying
+  // thread even if it has terminated itself.
+  EXPECT_TRUE(thread->GetTaskRunner());
 
   // Calling |Start()| should fail because neither |Stop()| nor |join()| have
   // been called yet.
